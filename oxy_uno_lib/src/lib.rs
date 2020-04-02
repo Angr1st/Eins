51542ea4c9c716c9fec93a259d8c56a7083b7d4a
Torben Clasen <t.clasen@outlook.com>
2020-04-02 22:25:51 +0200

    first version of all card related types

--- oxy_uno_lib/src/lib.rs
+++ oxy_uno_lib/src/lib.rs
@@ -1,7 +1,75 @@
+#[macro_use]
+extern crate specs_derive;
+
+pub mod cards {
+    use specs::prelude::*;
+
+    pub const MAX_CARD_NUMBER: i32 = 108;
+
+    #[derive(Debug)]
+    pub enum Color{
+        Red,
+        Blue,
+        Orange,
+        Green
+    }
+
+    #[derive(Debug)]
+    pub enum CardNumbers {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine
+    }
+
+    #[derive(Debug)]
+    pub enum Symbol{
+        Number(CardNumbers),
+        Draw,
+        Wild,
+        Reverse,
+        Skip
+    }
+
+    #[derive(Component)]
+    pub struct Reversable {}
+
+    #[derive(Component)]
+    pub struct Skip {}
+
+    #[derive(Component)]
+    pub struct Wild {
+        pub next_color: Color
+    }
+
+    #[derive(Component)]
+    pub struct Drawable {
+        pub number_of_cards_to_draw: DrawAction
+    }
+
+    #[derive(Debug)]
+    pub enum DrawAction{
+        DrawOne,
+        DrawTwo,
+        DrawFour
+    }
+
+    #[derive(Debug)]
+    pub enum Category{
+
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn number_is_108() {
+        assert_eq!(108, crate::cards::MAX_CARD_NUMBER);
     }
 }
