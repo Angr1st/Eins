958426b1b9495e8e137a1726947d3d30a46bce55
Torben Clasen <t.clasen@outlook.com>
2023-01-20 20:20:56 +0100

    wip

--- eins_lib/src/lib.rs
+++ eins_lib/src/lib.rs
@@ -1,10 +1,11 @@
 mod game;
 mod cards;
 
+use cards::CardTypes;
 use game::{GameSession, Hand};
 
-pub fn test() -> GameSession<'static> {
-    let cards= cards::init_deck();
+pub fn test(cards: &'static Vec<CardTypes>) -> GameSession<'static> {
+
     let hand_one = Hand::new(None);
     let hand_two = Hand::new(None);
     let players = vec![hand_one, hand_two];
