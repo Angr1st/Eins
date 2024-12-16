3a3ab7112be661e6ba1a924f7c3c6af2163e5876
Torben Clasen <t.clasen@outlook.com>
2024-12-16 18:36:33 +0100

    player code now starts with the id of the player; added function to retrieve id from code;

--- eins_lib/src/infrastructure.rs
+++ eins_lib/src/infrastructure.rs
@@ -14,11 +14,12 @@ pub struct Player {
 
 impl Player {
     pub fn new(nick: String) -> Self {
+        let id = Uuid::new_v4();
         let mut code = String::new();
-        Player::generate_code(&mut code);
+        Player::generate_code(&id, &mut code);
         Self {
             nick,
-            id: Uuid::new_v4(),
+            id,
             last_activity: Timestamp::now(),
             code,
         }
@@ -36,10 +37,20 @@ impl Player {
         &self.code
     }
 
-    fn generate_code(buffer: &mut String) {
-        for _ in 0..4 {
+    fn generate_code(id: &Uuid, buffer: &mut String) {
+        buffer.push_str(&id.to_string());
+        for _ in 0..3 {
             buffer.push_str(&Uuid::new_v4().to_string())
         }
+    }
+
+    pub fn get_id_from_code(code: &str) -> Option<Uuid> {
+        if code.len() < 36 {
+            return None;
+        }
+        let first_uuid = &code[0..36];
+        let first_uuid = Uuid::parse_str(first_uuid);
+        first_uuid.ok()
     }
 }
 
